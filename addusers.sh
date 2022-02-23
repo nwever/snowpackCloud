de12646f19496a14644fee52e6f140930df398ef
Nander Wever <nwever@gmail.com>
2022-02-23 15:43:27 +0000

    Adding automatization script for adding users.

---
+++ addusers.sh
@@ -0,0 +1,20 @@
+# Add users listed in users.lst
+while read user
+do
+	echo "Adding: ${user}"
+	# Create user on system
+	sudo useradd -s /usr/bin/bash -d /home/jupyter-${user} -m jupyter-${user}
+	# Add user to tljh
+	sudo tljh-config add-item users.allowed ${user}
+	# Copy over the repository
+	sudo cp -Rp . /home/jupyter-${user}/
+	# Remove files that the user doesn't need
+	sudo rm /home/jupyter-${user}/users.lst
+	sudo rm /home/jupyter-${user}/addusers.sh
+	sudo rm /home/jupyter-${user}/README.md
+	sudo rm /home/jupyter-${user}/setup.sh
+	# Change file ownership to the user
+	sudo chown -R jupyter-${user}:jupyter-${user} /home/jupyter-${user}/
+done < users.lst
+# Reload tljh with new userbase
+sudo tljh-config reload
