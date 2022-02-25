a98eb9c74ad7479a029237df3fa3f3edc17a8924
Nander Wever <nwever@gmail.com>
2022-02-25 23:03:47 +0000

    Reformatting the Dry Lake *.smet file such that it is formatted similarly to the other *.smet files and can be perturbed by the run_SNOWPACK.sh script. Also set the ProfileDate for Dry Lake to the current water year. Making clear in the notebook which variables for Dry Lake can be perturbed.

--- update_drylake/make_smet.sh
+++ update_drylake/make_smet.sh
@@ -7,10 +7,11 @@ echo "longitude     = 106.78333333333333" >> drylake.smet
 echo "altitude      = 2560.32" >> drylake.smet
 echo "nodata        = -999" >> drylake.smet
 echo "tz            = -6" >> drylake.smet
-echo "fields        = timestamp TA RH VW ISWR HS PSUM_UNHEATED" >> drylake.smet
-echo "units_offset  = 0         0  0  0  0    0  0" >> drylake.smet
+echo "fields        = timestamp TA RH VW DW P PSUM_UNHEATED HS ISWR ILWR" >> drylake.smet
+echo "units_multiplier = 1 1 1 1 1 1 1 1 1 1" >> drylake.smet
+echo "units_offset = 0 0 0 0 0 0 0 0 0 0" >> drylake.smet
 echo "[DATA]" >> drylake.smet
 # Without filters:
 #grep -h ^457 [0-9[0-9][0-9][0-9].csv | mawk -F, '{psum=($5>0)?($5*25.4*0.5):(0.); if(prev_sum>psum) {prev_sum=psum}; print substr($2,1,10) "T" substr($3,1,5), $7+273.15, $33/100, $30*0.44704, $32, ($8<0)?(0.):($8*0.0254), psum-prev_sum; prev_sum=psum}' >> drylake.smet
 # With filters:
-grep -h ^457 [0-9][0-9][0-9][0-9].csv | mawk -F, '{psum=($5>0)?($5*25.4*0.5):(0.); if(prev_sum>psum) {prev_sum=psum}; print substr($2,1,10) "T" substr($3,1,5), ($7>-50 && $7<50)?($7+273.15):(-999), ($33>2 && $33<=100)?($33/100):(-999), ($30>=0 && $30<=100)?($30*0.44704):(-999), ($32>=0 && $32<1500)?($32):(-999), ($8<0)?(0.):($8*0.0254), psum-prev_sum; prev_sum=psum}' >> drylake.smet
+grep -h ^457 [0-9][0-9][0-9][0-9].csv | mawk -F, '{psum=($5>0)?($5*25.4*0.5):(0.); if(prev_sum>psum) {prev_sum=psum}; print substr($2,1,10) "T" substr($3,1,5), ($7>-50 && $7<50)?($7+273.15):(-999), ($33>2 && $33<=100)?($33/100):(-999), ($30>=0 && $30<=100)?($30*0.44704):(-999), -999, -999, psum-prev_sum, ($8<0)?(0.):($8*0.0254), ($32>=0 && $32<1500)?($32):(-999), -999; prev_sum=psum}' >> drylake.smet
