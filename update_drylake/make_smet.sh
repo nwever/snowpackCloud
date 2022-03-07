ced6d27076039607b4d035cb65b83c1ef2e48722
Nander Wever <nwever@gmail.com>
2022-03-07 18:23:55 +0000

    Updating the scripts to retrieve and make *.smet files for both Dry Lake and Lake Eldora SNOTEL sites.

--- update_drylake/make_smet.sh
+++ update_drylake/make_smet.sh
@@ -14,4 +14,20 @@ echo "[DATA]" >> drylake.smet
 # Without filters:
 #grep -h ^457 [0-9[0-9][0-9][0-9].csv | mawk -F, '{psum=($5>0)?($5*25.4*0.5):(0.); if(prev_sum>psum) {prev_sum=psum}; print substr($2,1,10) "T" substr($3,1,5), $7+273.15, $33/100, $30*0.44704, $32, ($8<0)?(0.):($8*0.0254), psum-prev_sum; prev_sum=psum}' >> drylake.smet
 # With filters:
-grep -h ^457 [0-9][0-9][0-9][0-9].csv | mawk -F, '{psum=($5>0)?($5*25.4*0.5):(0.); if(prev_sum>psum) {prev_sum=psum}; print substr($2,1,10) "T" substr($3,1,5), ($7>-50 && $7<50)?($7+273.15):(-999), ($33>2 && $33<=100)?($33/100):(-999), ($30>=0 && $30<=100)?($30*0.44704):(-999), -999, -999, psum-prev_sum, ($8<0)?(0.):($8*0.0254), ($32>=0 && $32<1500)?($32):(-999), -999; prev_sum=psum}' >> drylake.smet
+grep -h ^457 457_[0-9][0-9][0-9][0-9].csv | mawk -F, '{psum=($5>0)?($5*25.4*0.5):(0.); if(prev_sum>psum) {prev_sum=psum}; print substr($2,1,10) "T" substr($3,1,5), ($7>-50 && $7<50)?($7+273.15):(-999), ($33>2 && $33<=100)?($33/100):(-999), ($30>=0 && $30<=100)?($30*0.44704):(-999), -999, -999, psum-prev_sum, ($8<0)?(0.):($8*0.0254), ($32>=0 && $32<1500)?($32):(-999), -999; prev_sum=psum}' >> drylake.smet
+
+
+
+echo "SMET 1.1 ASCII" > lakeeldora.smet
+echo "[HEADER]" >> lakeeldora.smet
+echo "station_id    = lakeeldora" >> lakeeldora.smet
+echo "station_name  = lakeeldora" >> lakeeldora.smet
+echo "latitude      = 40.53333333333333" >> lakeeldora.smet
+echo "longitude     = 106.78333333333333" >> lakeeldora.smet
+echo "altitude      = 2560.32" >> lakeeldora.smet
+echo "nodata        = -999" >> lakeeldora.smet
+echo "tz            = -6" >> lakeeldora.smet
+echo "fields        = timestamp HS PSUM_UNHEATED" >> lakeeldora.smet
+echo "units_offset  = 0         0  0" >> lakeeldora.smet
+echo "[DATA]" >> lakeeldora.smet
+grep -h ^564 564_[0-9][0-9][0-9][0-9].csv | mawk -F, '{psum=($5>0)?($5*25.4*0.5):(0.); if(prev_sum>psum) {prev_sum=psum}; print substr($2,1,10) "T" substr($3,1,5), ($7<0)?(0.):($7*0.0254), psum-prev_sum; prev_sum=psum}' >> lakeeldora.smet
--- update_drylake/retrieve_data.sh
+++ update_drylake/retrieve_data.sh
@@ -4,13 +4,14 @@
 #   3) After the download starts, check in the debugger under the tab "Network", the POST command. Right click --> copy --> Copy as Curl
 
 # Historic water years
-for yr in 2020 2021
+for stn in 457 564
 do
-	current=$(echo ${yr} | mawk '')
+	for yr in 2020 2021
+	do
+		curl 'https://wcc.sc.egov.usda.gov/nwcc/view' -X POST -H 'Content-Type: application/x-www-form-urlencoded' -H 'Origin: https://wcc.sc.egov.usda.gov' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Referer: https://wcc.sc.egov.usda.gov/nwcc/site?sitenum='${stn}'' --data-raw 'intervalType=+View+Historic+&report=ALL&timeseries=Hourly&format=copy&sitenum='${stn}'&interval=WATERYEAR&year='${yr}'&month=WY&userEmail=' > ${stn}_${yr}.csv
+	done
 
-	curl 'https://wcc.sc.egov.usda.gov/nwcc/view' -X POST -H 'Content-Type: application/x-www-form-urlencoded' -H 'Origin: https://wcc.sc.egov.usda.gov' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Referer: https://wcc.sc.egov.usda.gov/nwcc/site?sitenum=457' --data-raw 'intervalType=+View+Historic+&report=ALL&timeseries=Hourly&format=copy&sitenum=457&interval=WATERYEAR&year='${yr}'&month=WY&userEmail=' > ${yr}.csv
+	# Current water year
+	let yr=${yr}+1
+	curl 'https://wcc.sc.egov.usda.gov/nwcc/view' -X POST -H 'Origin: https://wcc.sc.egov.usda.gov' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Referer: https://wcc.sc.egov.usda.gov/nwcc/site?sitenum='${stn}'' --data-raw 'intervalType=+View+Current+&report=ALL&timeseries=Hourly&format=copy&sitenum='${stn}'&interval=WATERYEAR&year='${yr}'&month=WY&userEmail=' > ${stn}_${yr}.csv
 done
-
-# Current water year
-let yr=${yr}+1
-curl 'https://wcc.sc.egov.usda.gov/nwcc/view' -X POST -H 'Origin: https://wcc.sc.egov.usda.gov' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Referer: https://wcc.sc.egov.usda.gov/nwcc/site?sitenum=457' --data-raw 'intervalType=+View+Current+&report=ALL&timeseries=Hourly&format=copy&sitenum=457&interval=WATERYEAR&year='${yr}'&month=WY&userEmail=' > ${yr}.csv
